e9bf96dc52ac2c785e03aee4d63d2049402054da
hxshandle <handle.huang0409@gmail.com>
2015-01-06 21:48:23 +0800

    add koa-userauth

--- koa-app/app.js
+++ koa-app/app.js
@@ -1,29 +1,45 @@
 "use strict";
-
 var app = module.exports = require('koa')();
 var favicon = require('koa-favicon'),
-router = require('koa-route'),
-logger = require('koa-logger'),
-session = require('koa-generic-session'),
-redisStore = require('koa-redis');
+  router = require('koa-route'),
+  logger = require('koa-logger'),
+  session = require('koa-generic-session'),
+  userauth = require('koa-userauth'),
+  redisStore = require('koa-redis');
+
 
 app.keys = ["I'm a key"];
 app.use(logger());
 app.use(favicon(__dirname + '/public/favicon.ico'));
-//session
+// Session
 app.use(session({
   //store: redisStore()
 }));
+// User Auth
+app.use(userauth({
+  match: '',
+  // auth system login url
+  loginURLFormater: function (url,rootPath) {
+    return '/login?redirect=' + url;
+  },
+  // login callback and getUser info handler
+  getUser: function* (ctx) {
+    var token = ctx.query.token;
+    // get user
+    return null;
+  }
+}));
+
 
 app.use(function * (next) {
-  this.body = "Helloaaa World";
+  this.body = "Hello World";
   var session = this.session;
   session.abc = "abc";
   session.ad = "ad";
   yield next;
 });
 
+
 if (!module.parent) {
   app.listen(3000);
 }
-
