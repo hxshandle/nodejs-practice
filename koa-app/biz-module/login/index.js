e5a1671ca5e2bc283d2cb1674babc796c3a11ba5
Handle <handle.huang@mac15>
2015-01-14 16:04:43 +0800

    add view support

--- koa-app/biz-module/login/index.js
+++ koa-app/biz-module/login/index.js
@@ -1,6 +1,19 @@
-module.exports = function(){
-  return function *(next){
-    this.body = "this is login";
-    yield *next;
+var render = require('co-render'),
+  join = require('path').join,
+  debug = require('debug')('koa-jenkins:biz:login');
+
+module.exports = function() {
+  return function * (next) {
+    var view = join(__dirname, 'template', 'index.jade');
+    debug('view is %s', view);
+    var options = {
+      cache: true,
+      filename: view,
+      timestamp: new Date()
+    };
+    var t1 = (new Date()).getTime();
+    this.body = yield render(view, options);
+    debug('render %s jade cost %s', view, (new Date).getTime() - t1);
+    yield * next;
   }
 }();
